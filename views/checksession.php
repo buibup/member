372d6c4e9934df4ae6596e2f8b1c64e350621fc2
buibup <b5209194@gmail.com>
2015-11-29 20:12:18 +0700

    new commit

--- views/checksession.php
+++
@@ -1,8 +0,0 @@
-<?
-session_start();
-$sess_userid=$_SESSION[sess_userid];
-$sess_username=$_SESSION[sess_username];
-if($sess_userid<>session_id() || $sess_username==""){
-	header("Location : index.html"); exit();
-}
-?>
--- views/logout.php
+++
@@ -1,7 +0,0 @@
-<?
-	session_start();
-	unset($_SESSION['sess_userid']);
-	unset($_SESSION['sess_username']);
-	session_destroy();
-	header('Location: index.html');
-?>
